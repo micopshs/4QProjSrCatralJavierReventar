5bcdf44d33dc2392f3d3ea44111006b1295700d1
micopshs <90668033+micopshs@users.noreply.github.com>
2022-05-11 14:42:37 +0000

    Revamped formatting and added comments

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,6 @@
+/**
+ * Word Arrays to be used
+ */
 const FOUR_LETTER_WORDS = [
   "boss",
   "cast",
@@ -13,15 +16,15 @@ const FOUR_LETTER_WORDS = [
 
 const FIVE_LETTER_WORDS = [
   "amber",
-  //"broad",
-  //"depth",
-  //"fixed",
-  //"panel",
-  //"quack",
-  //"rural",
-  //"seven",
-  //"thief",
-  //"unity"
+  "broad",
+  "depth",
+  "fixed",
+  "panel",
+  "quack",
+  "rural",
+  "seven",
+  "thief",
+  "unity"
 ];
 
 const SIX_LETTER_WORDS = [
@@ -50,6 +53,9 @@ const SEVEN_LETTER_WORDS = [
   "weather"
 ];
 
+/**
+ * Redirects to index.html if game settings are not loaded
+ */
 function redirect() {
   if (Object.keys(localStorage).length == 0) {
     alert("Game settings not loaded! Redirecting you to homepage");
@@ -59,56 +65,69 @@ function redirect() {
   return true;
 }
 
+/**
+ * "Dummy" function used to trigger the onbeforeunload event when pressing the navigation buttons
+ */
 function warn() {
   return confirm("");
 }
 
-
+/**
+ * Adds the user-entered word bank to the custom list
+ * @param { String } str - a string representing the textarea value of the word bank
+ */
 function loadCustomList(str) {
+  //Splits the gathered text by a space pattern
   let listArray = str.split(/(\s+)/);
   let outputArray = []
-
+  //Removes space characters from the wordArray
   for (let i = 0; i < listArray.length; i++) {
-    if (listArray[i].includes(" ")) 
+    if (listArray[i].includes(" "))
       listArray.splice(i, 1);
   }
-
+  //Pushes each word to the output array
   for (let i = 0; i < listArray.length; i++) {
-    if (listArray[i] == '') continue;
+    if (listArray[i] == '') continue; //Note: see line 116 of index.js for this issue
     outputArray.push(listArray[i]);
   }
 
   return outputArray;
 }
 
+/**
+ * Loads the appropriate game settings
+ */
 function game() {
   let rightWord, rightWordList;
-
+  //Sets the default word bank/list
   switch (Number.parseInt(JSON.parse(localStorage.getItem("settings"))["wordLength"])) {
     case 4: rightWordList = FOUR_LETTER_WORDS; break;
     case 5: rightWordList = FIVE_LETTER_WORDS; break;
     case 6: rightWordList = SIX_LETTER_WORDS; break;
     case 7: rightWordList = SEVEN_LETTER_WORDS; break;
   }
-
- rightWordList = rightWordList.concat(loadCustomList(JSON.parse([localStorage.getItem("settings")])["wordPool"]));
+  //Concatenates the user-entered and default word banks/lists
+  rightWordList = rightWordList.concat(loadCustomList(JSON.parse([localStorage.getItem("settings")])["wordPool"]));
+  //Randomizes the correct word
   rightWord = rightWordList[Math.floor(Math.random() * rightWordList.length)];
-  
-  sessionStorage.setItem("attempt", "");
-  sessionStorage.setItem("attemptCount", JSON.parse(1));
-  sessionStorage.setItem("position", JSON.parse(1));
-  sessionStorage.setItem("rightWord", rightWord);
-  sessionStorage.setItem("isFinished", JSON.parse(false));
-  
+  //Sets the game 'variables'
+  sessionStorage.setItem("attempt", "");                    //Contains the last attempted word
+  sessionStorage.setItem("attemptCount", JSON.parse(1));    //Contains the number of attempts
+  sessionStorage.setItem("position", JSON.parse(1));        //Contains the current position of the user
+  sessionStorage.setItem("rightWord", rightWord);           //Contains the correct word
+  sessionStorage.setItem("isFinished", JSON.parse(false));  //Contains the boolean the determines if the game is finished
+  //Generates the grid
   generateGrid();
 }
 
+/**
+ * Generates the playing grid
+ */
 function generateGrid() {
+  //Variables that determine its size
   let maxRows = JSON.parse(localStorage.getItem("settings"))["maxAttempts"];
   let maxCols = JSON.parse(localStorage.getItem("settings"))["wordLength"];
-  
-  //if (!maxRows) maxRows = 1;
-  
+  //Nested loop to insert each cell and row
   for (let i = 1; i <= maxRows; i++) {
     let row = document.createElement("div");
     row.id = `l${i}0`;
@@ -123,88 +142,101 @@ function generateGrid() {
   }
 }
 
+/**
+ * Adds a letter on the grid
+ */
 function addLetter(btn) {
   let position = Number.parseInt(JSON.parse(sessionStorage.getItem("position")));
   let wordLength = Number.parseInt(JSON.parse(localStorage.getItem("settings"))["wordLength"]);
   let isFinished = JSON.parse(sessionStorage.getItem("isFinished"));
-
+  //User cannot type after the game is finished
   if (isFinished) return;
-  
+  //If the position exceeds wordLength, then no event will happen
   if (position <= wordLength) {
     let attempt = sessionStorage.getItem("attempt");
     let attemptCount = Number.parseInt(JSON.parse(sessionStorage.getItem("attemptCount")));
     let cell = document.getElementById(`l${attemptCount}${position}`);
-    
+    //Enters the letter from the button clicked
     cell.innerHTML = btn.innerHTML;
+    //Increments position
     sessionStorage.setItem("position", JSON.parse((position + 1)));
+    //Adds char to the end of the attempted word string
     sessionStorage.setItem("attempt", (attempt += cell.innerHTML));
   }
 }
 
+/**
+ * Removes a letter on the grid
+ */
 function popLetter() {
   let attempt = sessionStorage.getItem("attempt");
   let attemptCount = Number.parseInt(JSON.parse(sessionStorage.getItem("attemptCount")));
   let position = Number.parseInt(JSON.parse(sessionStorage.getItem("position")));
   let isFinished = JSON.parse(sessionStorage.getItem("isFinished"));
-  
+  //User cannot remove letter if there's no letter on the row or the game is finished
   if (position == 1 || isFinished) return;
-  
+  //Removes the letter content of the cell
   let cell = document.getElementById(`l${attemptCount}${(position - 1)}`);
   cell.innerHTML = "";
+  //Decrements position
   sessionStorage.setItem("position", JSON.parse((position - 1)));
+  //Remove char from the end of the attempted word string
   sessionStorage.setItem("attempt", attempt.slice(0, -1));
-} 
+}
 
+/**
+ * Validates attempted word
+ */
 function validateAttempt() {
-  let isFinished = JSON.parse(sessionStorage.getItem("isFinished")); 
+  let isFinished = JSON.parse(sessionStorage.getItem("isFinished"));
   let wordLength = Number.parseInt(JSON.parse(localStorage.getItem("settings"))["wordLength"]);
   let position = Number.parseInt(JSON.parse(sessionStorage.getItem("position")));
   let attempt = sessionStorage.getItem("attempt");
   let attemptCount = Number.parseInt(JSON.parse(sessionStorage.getItem("attemptCount")));
   let maxAttempts = Number.parseInt(JSON.parse(localStorage.getItem("settings"))["maxAttempts"]);
   let rightWord = sessionStorage.getItem("rightWord").toUpperCase();
-
+  let hintsDisabled = JSON.parse(localStorage.getItem("settings"))["hints"];
+  //Does not validate attempt when game is finished
   if (isFinished) return
-
+  //Alerts user if they still have missing letters
   if (position != wordLength + 1) {
     alert(`Please type ${ wordLength + 1 - position } more letter/s`);
     return;
   }
-
-  console.log(`Attempt ${attemptCount}`)
-
+  //Loop checks each char
   for (let i = 0; i < wordLength; i++) {
     let attemptChar = attempt[i];
     let rightChar = rightWord[i];
     let element = document.getElementById(`l${attemptCount}${i + 1}`);
-
-    if (attemptChar == rightChar) 
+    //If char is correct and in the correct position
+    if (attemptChar == rightChar)
       element.style.backgroundColor = "var(--guess-right-place)";
-    else if (rightWord.includes(attemptChar)) 
+    //If char is correct and in the wrong position (may be disabled by the user)
+    else if (rightWord.includes(attemptChar) && !hintsDisabled)
       element.style.backgroundColor = "var(--guess-wrong-place)";
-    else 
+    //If char is not found on the word
+    else
       element.style.backgroundColor = "var(--guess-wasted)";
-
+    //Miscellaneous CSS
     element.style.fontWeight = "bold";
     element.style.color = "var(--guess-text-color)";
   }
-
+  //Event when user guesses the right word
   if (attempt == rightWord) {
     sessionStorage.setItem("isFinished", JSON.parse(true));
-
     // this will be used in the finished_js file
     localStorage.setItem("attempts_taken", attemptCount);
     localStorage.setItem("attempts_possible", maxAttempts);
     // redirect to the next page
     window.location.href = "../htdocs/finished.html";
     return;
-  } 
+  }
 
-  if (attemptCount + 1 <= maxAttempts) {
-    sessionStorage.setItem("attempt", "");
-    sessionStorage.setItem("attemptCount", JSON.parse(attemptCount + 1));
-    sessionStorage.setItem("position", JSON.parse(1));
-  } else {
+  if (attemptCount + 1 <= maxAttempts) { //when user still has spare attempts
+    sessionStorage.setItem("attempt", "");                                  //Reset attempted word
+    sessionStorage.setItem("attemptCount", JSON.parse(attemptCount + 1));   //Increments attemptCount
+    sessionStorage.setItem("position", JSON.parse(1));                      //Reset position back to 1
+  } else {  //when user has no more attempts
     sessionStorage.setItem("isFinished", JSON.parse(true));
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,73 @@
+/**
+ * Saves settings if nothing is stored in localStorage upon loading
+ */
+function loadDefault() {
+  if (Object.keys(localStorage).length == 0) saveSettings(settingsForm);
+}
+
+/**
+ * Alerts the user by displaying a red border if input does not follow constraints.
+ * @param { HTMLInputElement } input - the target input element
+ */
 function validateInput(input) {
   let condition = (
-    Number.parseInt(input.value) <= input.max && 
-    Number.parseInt(input.value) >= input.min &&
-    input.value % 1 == 0
+    Number.parseInt(input.value) <= input.max && //value should be less than or equal to the maximum
+    Number.parseInt(input.value) >= input.min && //value should be greater than or equal to the minimum
+    input.value % 1 == 0                         //value should have no decimals
   );
   input.style.border = condition ? "2px solid green" : "2px solid red";
-} 
+}
 
-
+/**
+ * Switches the visibilibity of a dialog element
+ * @param { HTMLDialogElement } target - the target dialog element
+ */
 function toggleVisibility(target) {
   target.open = !(target.open)
+  //Darkens the background when disabled to avoid user distraction
   if (target.open) document.body.style.background = "rgba(0, 0, 0, 0.7)";
   else document.body.style.background = "";
 }
 
+/**
+ * Allows for enabling/disabling an input field .
+ * @param { HTMLInputElement } target- the target input element
+ */
 function toggleField(target) {
   target.disabled = !(target.disabled);
+  //Searches for the label of the target input element
   let targetLabel = document.querySelector(`label[for='${target.id}']`);
-
+  //Puts a strike-through style on the label
   targetLabel.style.textDecoration = target.disabled ? "line-through" : "none";
 }
 
+/**
+ * Sets the acceptable max attempt range to [n, 2n], where n is the chosen word length
+ */
 function setMaxAttemptBounds() {
   var min = wordLength.value;
   var max = 2 * min;
   //Change input field attr
   maxAttempts.min = min;
   maxAttempts.max = max;
-  // Change display
+  //Change display
   minBound.innerHTML = min;
   maxBound.innerHTML = max;
-
+  //Validates input
   validateInput(maxAttempts);
 }
 
+/**
+ * Validates and saves the data collected from the form into localStorage
+ * @param { HTMLFormElement } form - the target form element
+ */
 function saveSettings(form) {
+  // Validates the inputted word bank
   if (!validateWordBank()) {
     toggleVisibility(settings);
     return false;
   }
-  // Utility func to form data to object format
+  // Utility func to transform form data to object format
   const getFormJSON = (form) => {
     const data = new FormData(form);
     return Array.from(data.keys()).reduce((result, key) => {
@@ -49,15 +77,18 @@ function saveSettings(form) {
   };
 
   let formObject = getFormJSON(form);
-  // store the 2 colors in the necessary variables
+  //Store the 2 colors in the necessary variables
   document.documentElement.style.setProperty("--guess-right-place", formObject["correctColor"]);
   document.documentElement.style.setProperty("--guess-wrong-place", formObject["hintColor"]);
-  // store the entire form data to local storage
+  //Store the entire form data to local storage
   localStorage.setItem("settings", JSON.stringify(formObject));
   return false;
 }
 
-function resetSettings(form) {
+/**
+ * Alerts user before resetting and resets border style of wordLength and maxAttempts input elements
+ */
+function resetSettings() {
   if (confirm("Reset form? All your changes will be lost!")) {
     wordLength.border = "2px solid green";
     maxAttempts.border = "2px solid green";
@@ -66,22 +97,33 @@ function resetSettings(form) {
   return false;
 }
 
+/**
+ * Ensures the words in the wordbank follows specified constraints
+ */
 function validateWordBank() {
+  //Regex patterns
   let lettersPattern = /^[a-zA-Z]+$/;
   let spacesPattern = /(\s+)/;
+  //Splits the gathered text by using spacesPattern
   let wordArray = wordPool.value.split(spacesPattern);
-  
+  //Removes space characters from the wordArray
   for (let i = 0; i < wordArray.length; i++) {
-    if (wordArray[i].includes(" ")) 
+    if (wordArray[i].includes(" "))
       wordArray.splice(i, 1);
   }
-
+  //Traverses through each word
   for (let i = 0; i < wordArray.length; i++) {
+    /* Note: for some reason, when a user enters an input with excess spaces, the
+    wordArray (despite being filtered from space chars) leaves an empty string element
+    at the end. As such, we will just ignore it in validation */
     if (wordArray[i] == '') continue;
+    //Tests if the words entered only consist of letters in the english alphabet
     if (!(lettersPattern.test(wordArray[i]))) {
-      alert(`Sorry, but words like ${ wordArray[i] } are not valid since they contain characters that are not letters`);
+      alert(`Sorry, but words like ${ wordArray[i] } are not valid since they
+        contain characters that are not letters`);
       return false;
     }
+    //Tests if the words entered follow the specified word length
     if (wordArray[i].length != wordLength.value) {
       alert(`${ wordArray[i] } is not a ${ wordLength.value }-letter word`);
       return false;
@@ -89,8 +131,4 @@ function validateWordBank() {
   }
 
   return true;
-}
-
-function loadDefault() {
-  if (Object.keys(localStorage).length == 0) saveSettings(settingsForm);
 }
